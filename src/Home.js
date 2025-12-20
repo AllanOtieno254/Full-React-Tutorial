f0cb28bf48dcc19d15938db4d1714b380e235b8c
allan <allanotieno2001@gmail.com>
2025-12-20 10:44:35 +0300

    initial commit

--- src/Home.js
+++ src/Home.js
@@ -46,24 +46,74 @@
 // export default Home;
 
 
+// import { useState } from "react";
+// const Home = () => {
+// const[blogs,setBlogs]= useState([
+//     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+//     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+//     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+//   ])
+
+//   return (
+//     <div className="home">
+//       {blogs.map((blog) => (
+//         <div className="blog-preview" key={blog.id} >
+//           <h2>{ blog.title }</h2>
+//           <p>Written by {blog.author}</p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+ 
+// export default Home;
+
+
+
+// import { useState } from "react";
+// import BlogList from "./BlogList";
+// const Home = () => {
+// const[blogs,setBlogs]= useState([
+//     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+//     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+//     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+//   ])
+
+//   return (
+//     <>
+//       <BlogList blogs={blogs} title="All Blogs" />
+//       <BlogList blogs={blogs.filter(blog => blog.author === 'mario')} title="Mario's Blogs" />
+//     </>
+//   );
+// }
+
+// export default Home;
+
+
+
+
 import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-const[blogs,setBlogs]= useState([
+  const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
   return (
     <div className="home">
-      {blogs.map(blog => (
-        <div className="blog-preview" key={blog.id} >
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
   );
 }
  
 export default Home;
+
+
